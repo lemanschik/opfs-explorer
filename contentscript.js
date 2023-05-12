3783f52ddc32eabe4fefa7e2a6560407acd27bf5
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-12 14:48:29 +0200

    Update contentscript.js
    some improvments and a small fs.api

--- contentscript.js
+++ contentscript.js
@@ -2,7 +2,7 @@ export const handelsByPathname = {}
 
 export const getRelativePath = async (handle) => `./${(await root.resolve(handle)).join('/')}`;    
 
-export const getDirectoryEntriesRecursive = (baseUrl,handle,sendResponse) => {
+export const getDirectoryEntriesRecursive = (baseUrl='.',handle,sendResponse) => {
   const pathname = `${baseUrl}/${handle.name}`;
   // Storing handels by pathname for later lookups.
   handelsByPathname[pathname] = handle;
@@ -33,27 +33,41 @@ export const getDirectoryEntriesRecursive = (baseUrl,handle,sendResponse) => {
   return getEntrie[handle.kind]();
 };
 
-export const mkdirP = async (pathname) => pathname.split('/').reduce(
-  (dir,dirName)=>dir.getDirectoryHandle(dirName,{ create: true }),
-  await navigator.storage.getDirectory()
-);
+// [directoryName,fileName];
+const parsePath = (pathname) => [pathname.slice(0,pathname.lastIndexOf('/'),pathname.slice(pathname.lastIndexOf('/')+1];
 
-export const readdir = async (pathname) => pathname.split('/').reduce(
-  async (dir,dirName) => await dir?.getDirectoryHandle(dirName,{ create: false }),
+export const mkdirP = (pathname) => readdir(pathname,{create:true});
+
+export const readdir = async (pathname,options={ create: false }) => await pathname.split('/').reduce(
+  async (dir,dirName) => (dir = await dir.getDirectoryHandle(dirName,options)),
   await navigator.storage.getDirectory()
 ).values;
 
-export const getFileHandle = async (pathname,options={create: false}) => await pathname.split('/').reduce(
-  async (dir,dirName) => await dir?.getDirectoryHandle(dirName,options),
-  await navigator.storage.getDirectory()
-).getFileHandle(pathname.slice(pathname.lastIndexOf('/')+1,options);
+// Can return file and Directory handels given a pathname
+export const getFileHandle = async (pathname,options={create: false}) => {
+  const [directoryName,fileName] = parsePath(pathname);
+  return await (await readdir(directoryName),options))
+    .getFileHandle(fileName),options);
+};
+
+// Can return a directory even if a filePath is given it returns the directoryHandle of the filePath
+export const getDirectoryHandle = async (pathname,options={create: false}) => 
+  await pathname.split('/').reduce(
+    async (dir,dirName) => (dir = await dir.getDirectoryHandle 
+      ? dir.getDirectoryHandle(dirName, options) 
+      : dir),
+    await navigator.storage.getDirectory()
+  );
 
 export const onrequest = async (request, sender, sendResponse) => {
   if (request.message?.startsWith('getDirectory')) {
     return await getDirectoryEntriesRecursive(
-      await navigator.storage.getDirectory(),request.data||'.',sendResponse
+      request.data || '.',
+      await navigator.storage.getDirectory(),
+      sendResponse
     );
   } 
+  
   if (request.message?.startsWith('save') === 'saveFile') {
     const fileHandle = handelsByPathname[request.data];
     try {
