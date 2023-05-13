38ae67404d6ea54ff12eecbd1dadb603d4dd5a8b
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-13 07:37:36 +0200

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -6,11 +6,14 @@ const getDirectoryHandle = async (pathname) => await pathname.split('/').reduce(
 );
 
 export const readdir = async (pathname,options={create: false, recursive: false}) => !options.create && options.recursive 
-  ? readdirRecursiv(pathname) : [pathname, (await getDirectoryHandle(pathname)).values];
+  ? readdirRecursiv(pathname) : (await getDirectoryHandle(pathname)).values;
+
+const resolvePathnameFromHandle = async (handle) => (await (await navigator.storage.getDirectory())
+  .resolve(handle)).join('/') + `/${handle.name}`
 
 export const readdirRecursiv = async (pathname) => (await readdir(pathname,{create:false})).flatMap(
   handle => handle.kind === 'directory' 
-    ? readdirRecursiv(`${pathname}/${handle.name}`)
+    ? [pathname, await readdirRecursiv(`${pathname}/${handle.name}`)]
     : handle
   )
 );
@@ -19,45 +22,51 @@ const getDirPathnamesRecursiv = async (pathname) => (await readdir(pathname,{cre
   .filter((handel) => handle.kind === 'directory')
   .map((dirHandle) => getDirPathnamesRecursiv(`${pathname}/${(await dirHandle).name}`));
 
+// Can returns exist fileHandel but can also create a fileHandle and path
+export const getFileHandleFrom = async (pathname,options={create:false}) => {
+  const [directoryName,fileName] = parsePath(pathname);
+  return await (await readdir(directoryName),options))
+    .getFileHandle(fileName),options);
+};
+
+// (pathname === '.' ||  pathname === '/' || pathname === './' )
 // Can return a directoryHandleEntrie even if a filePath is given it returns the directoryHandle of the filePath
 export const getDirectoryHandleEntrie = async (pathname = '.', options={create: false}) => 
-  (pathname === '.' ||  pathname === '/' || pathname === './' ) ? await navigator.storage.getDirectory() : await pathname.split('/').reduce(
-    async (dir,dirName) => (dir = await dir.getDirectoryHandle 
+  await pathname.split('/').reduce(
+    async (dir,dirName) => (dir = dir.getDirectoryHandle 
       ? [`${dir[0]}/${dirName}`, await dir[1].getDirectoryHandle(dirName, options)]
       : [dir[0], await dir[1]])],
     await navigator.storage.getDirectory()
   );
 
-const resolvePathnameFromHandle = async (handle) => (await (await navigator.storage.getDirectory())
-  .resolve(handle)).join('/') + `/${handle.name}`
+const getFileEntrie = async (pathname,handle) => {
+      const { name, kind } = handle;    
+      const { size, type, lastModified } = await handle.getFile();
+      return {
+          pathname,  
+          name, kind,
+          size, type, lastModified,
+      };
+};
 
-export const getDirectoryEntries = (baseUrl = '.', maybeHandle) => new ReadableStream({ 
+export const getDirectoryEntries = (baseUrl = '.') => new ReadableStream({ 
   async start(stream){
-    const getDirectoryHandleEntrieRecursive = async (baseUrl = '.', maybeHandle) => {
-      const [pathname, handle] = await maybeHandle 
-        ? [`${baseUrl}/${(await maybeHandle).name}`,await maybeHandle] 
-        : getDirectoryHandleEntrie(baseUrl,{create:false});
+    const getDirectoryHandleEntrieRecursive = async (baseUrl = '.') => {
+      const [pathname, handle] = getDirectoryHandleEntrie(baseUrl,{create:false});
      
       // Storing handels by pathname for later lookups.
       handelsByPathname[pathname] = await handle;
-     
+      
       const { name, kind } = handle;
       const getEntrie = {
-        async file() {
-          const { size, type, lastModified } = await handle.getFile();
-          const entrie = {
-              pathname,  
-              name, kind,
-              size, type, lastModified,
-          };
-
+        async file() {          
+          const entrie = await getFileEntrie(handle);
           stream.enqueue([pathname, entrie]);
           return [pathname,entrie];
         },
         async directory() {
-          return [name,{
-              pathname,
-              name, kind,
+          return [name, {
+              pathname, name, kind,
               entries: Object.fromEntries(await Promise.all((await handle.values()).map(async (nextHandle)=>
                 await getDirectoryHandleEntrieRecursive(`${pathname}/${(await nextHandle).name}`, await nextHandle)()
               ))),
@@ -68,7 +77,7 @@ export const getDirectoryEntries = (baseUrl = '.', maybeHandle) => new ReadableS
       return getEntrie[handle.kind]();
     }
     
-    getEntriesRecursiv(baseUrl,maybeHandle);
+    getDirectoryHandleEntrieRecursive(baseUrl);
   }
 })
 
@@ -83,23 +92,14 @@ const parsePath = (pathname) => [
 
 
 export const mkdirP = (pathname) => readdir(pathname,{create:true});
-  
-// Can returns exist fileHandel but can also create a fileHandle and path
-export const getFileHandleFromPath = async (pathname,options={create:false}) => {
-  const [directoryName,fileName] = parsePath(pathname);
-  return await (await readdir(directoryName),options))
-    .getFileHandle(fileName),options);
-};
+
 
 export const onrequest = async (request, sender, sendResponse) => {
   if (request.message?.startsWith('getDirectory')) {
-    return getDirectoryEntriesRecursive(
-      request.data || '.',
-      getDirectoryHandle(request.data || '/'),
-      sendResponse
-    ).pipeTo(new WriteableStream({ write(entrie)}){ 
-      sendResponse({ entrie });
-    });
+    return getDirectoryEntriesRecursive(request.data)
+        .pipeTo(new WriteableStream({ write(entrie)}){ 
+          sendResponse({ entrie });
+        });
   } 
   
   if (request.message?.startsWith('save') === 'saveFile') {
